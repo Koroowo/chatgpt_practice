46393ae8168c72930993a9452520773d2f55af88
Koroowo <kcoolbrian@gmail.com>
2025-02-04 16:25:38 +0800

    Update add.php

---
+++ practice2/add.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>To-do網頁</title>
+    <link rel="stylesheet" href="bootstrap.css">
+    <link rel="stylesheet" href="index.css">
+    <script src="jquery.js"></script>
+</head>
+<body>
+    <div class="top_right_logo_div">
+        <img src="youyun.jpg" class="top_right_logo" alt="">
+    </div>
+    <div class="todo_title mx-auto">
+        <div class="todo_bar_div">
+            <div class="todo_bar"></div>
+            <div class="todo_bar"></div>
+            <div class="todo_bar"></div>
+        </div>
+        <h2 class="mx-auto text-white m-0">Add Todo</h2>
+        <button class="btn btn-danger" onclick="location.href='index.php'"></button>
+    </div>
+    <div class="todo_container mx-auto">
+        <div class="mt-4">
+            <input type="text" class="form-control w-50 mx-auto" id="task" placeholder="Task:" required >
+            <div class="d-flex justify-content-center align-items-center">
+                <button class="btn btn-success"></button>
+                <button type="button" class="btn btn-warning" onclick="clearinput();"></button>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
+<script>
+    function clearinput(){
+        document.getElementById("task").value="";
+    }
+</script>
